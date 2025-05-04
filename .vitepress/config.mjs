b3240645d2454e31ec5633040405797b68f310d7
hengqianfan <157280112+hengqianfan@users.noreply.github.com>
2025-05-05 01:32:04 +0800

    PRE:02
    q

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
-  base: 'vitepress-theme-fans-boke-mini-docs',
+  base: '/vitepress-theme-fans-boke-mini-docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
